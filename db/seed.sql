612be0bd43955057bcddae6296834284d179c27c
Neeraja Djearaman <neeru.jai@gmail.com>
2020-01-04 17:38:49 +0000

    add files

--- db/seed.sql
+++ db/seed.sql
@@ -15,18 +15,18 @@ INSERT INTO employee_role (title, salary, department_id) VALUES
     ('Managing Director', 120000, 1),
     ('Marketing Director', 80000, 2),
     ('Accounts Executive', 60000, 3),
-    ('HR Manager', 60000, 4,2),
-    ('HR Executive',40000,4,3),
-    ('Web Developer', 65000, 5,3),
-    ('Finance Director', 80000, 3,4),
-    ('Marketing Executive',40000,2,5),
-    ('Software Engineer', 74000, 5,1),
-    ('Tech Support', 45000, 5,1);
+    ('HR Manager', 60000, 4),
+    ('HR Executive',40000,4),
+    ('Web Developer', 65000, 5),
+    ('Finance Director', 80000, 3),
+    ('Marketing Executive',40000,2),
+    ('Software Engineer', 74000, 5),
+    ('Tech Support', 45000, 5);
 
 
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
-    ('Ryan', 'Hanks', 1 ),
+    ('Ryan', 'Hanks', 1,1 ),
     ('Juliana', 'Craig', 2, 3),
     ('Gloria', 'Hanks', 5, 1),
     ('Richard', 'Miller', 4, 1),
@@ -34,16 +34,16 @@ INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
     ('Goldie', 'Barns', 2, 5),
     ('Ryan', 'Cook', 4, 5),
     ('Kareena', 'Kapoor', 5, 5),
-    ('Bradley', 'White', 2),
-    ('John', 'Maisel', 3 ),
-    ('David ', 'Deigo', 3 ),
-    ('Astrid', 'Born', 5 ),
+    ('Bradley', 'White', 2,1),
+    ('John', 'Maisel', 3,1 ),
+    ('David ', 'Deigo', 3 ,2),
+    ('Astrid', 'Born', 5,3 ),
     ('Daisy', 'Goodman', 5, 4),
     ('Farah', 'Richards', 4, 4),
-    ('Kelvin', 'Costner', 2, 12),
+    ('Kelly', 'Costner', 2, 12),
     ('Paul', 'Conner', 1, 12),
-    ('Nadia', 'Williams', 4),
-    ('Scarlette', 'Moore', 5);
+    ('Nadia', 'Williams', 4,2),
+    ('Scarlette', 'Moore', 5,3);
 
 
 
